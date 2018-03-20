917f598b339fdd61203e710f4a69f7a2dc569ba2
James Howard <jameshoward@mac.com>
2018-03-20 12:31:42 -0700

    Initial Commit

---
+++ attachment-migrator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  attachment-migrator
+//
+//  Created by James Howard on 3/20/18.
+//  Copyright © 2018 Real Artists, Inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
